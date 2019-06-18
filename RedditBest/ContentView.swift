22521e0897166c38c19c67651d1d9b0d9108171c
Bhavik Soni <bhavik.s@canopas.com>
2019-06-18 11:06:12 +0530

    Initial Commit

---
+++ RedditBest/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  RedditBest
+//
+//  Created by Bhavik Soni on 18/06/19.
+//  Copyright © 2019 Bhavik Soni. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ RedditBestTests/RedditBestTests.swift
@@ -0,0 +1,34 @@
+//
+//  RedditBestTests.swift
+//  RedditBestTests
+//
+//  Created by Bhavik Soni on 18/06/19.
+//  Copyright © 2019 Bhavik Soni. All rights reserved.
+//
+
+import XCTest
+@testable import RedditBest
+
+class RedditBestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
